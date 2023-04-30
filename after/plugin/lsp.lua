ff42dcc4a2f2ce3708880838fec26ea96dae5e6d
Jorge Valdez <jorge@yung.dev>
2023-04-30 13:06:08 -0500

    fix completion cmp stuff in lsp file; move null ls to lsp file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -91,11 +91,29 @@ cmp.setup({
     mapping = {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<C-Space>"] = cmp.mapping.complete(),
+        --[[
         ["<Tab>"] = cmp_action.luasnip_supertab(),
         ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+        ]]--
     },
     preselect = 'item',
+    --[[
     completion = {
         completeopt = 'menu,menuone,noinsert'
+    },
+    ]]--
+});
+
+local null_ls = require("null-ls");
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.djhtml,
+        null_ls.builtins.formatting.djlint,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.pyproject_flake8,
+
+        null_ls.builtins.formatting.jq,
     },
 });
