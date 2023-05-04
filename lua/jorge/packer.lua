2563266d0c91e02864ff305c0e3929860a7ecbfa
Jorge Valdez <jorge@yung.dev>
2023-05-04 01:02:36 -0500

    add wilder

--- lua/jorge/packer.lua
+++ lua/jorge/packer.lua
@@ -93,6 +93,8 @@ return require('packer').startup(function(use)
 
     use({ 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' })
 
+    use({ 'gelguy/wilder.nvim' })
+
     use({
         "epwalsh/obsidian.nvim",
         config = function()
