b15a6ca568432fae2f65c4a442f517716c0ea397
Jorge Valdez <jorge@yung.dev>
2025-04-04 18:27:43 -0400

    reinstall avante it works now

--- lua/jorge/lazy.lua
+++ lua/jorge/lazy.lua
@@ -47,9 +47,9 @@ require("lazy").setup({
 			render_markdown = true,
 			which_key = true,
 		},
-        config = function()
-            vim.cmd.colorscheme("catppuccin-latte")
-        end
+		config = function()
+			vim.cmd.colorscheme("catppuccin-latte")
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -165,7 +165,7 @@ require("lazy").setup({
 		},
 		opts = {
 			ui = {
-				enable = false
+				enable = false,
 			},
 			dir = "~/obsidian/delvaze",
 			completion = {
@@ -264,8 +264,65 @@ require("lazy").setup({
 		-- use the `-` key to go up a directory
 		lazy = false,
 	},
+	--[[
 	{
 		"OXY2DEV/markview.nvim",
 		lazy = false,
+	},
+	--]]
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = "*",
+		opts = {
+			provider = "openrouter",
+			vendors = {
+				openrouter = {
+					__inherited_from = "openai",
+					endpoint = "https://openrouter.ai/api/v1",
+					api_key_name = "OPENROUTER_API_KEY",
+					model = "google/gemini-2.5-pro-preview-03-25",
+				},
+			},
+		},
+		build = "make",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			-- "zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
 	},
 })
