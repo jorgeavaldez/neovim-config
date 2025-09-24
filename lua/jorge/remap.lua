f546bf27a16a3286139a174e608a8fb4e4388e24
Jorge Valdez <jorge@yung.dev>
2025-09-24 16:19:39 -0400

    file clear binding

--- lua/jorge/remap.lua
+++ lua/jorge/remap.lua
@@ -24,6 +24,7 @@ vim.keymap.set("n", "<leader>ff", function()
 end)
 
 vim.keymap.set("n", "<leader>fc", ":call setreg('+', expand('%:.'))<CR>", { desc = "Copy file path" })
+vim.keymap.set("n", "<leader>fD", ":%d<CR>", { desc = "Delete contents of current buffer/file" })
 
 vim.keymap.set("n", "<C-k>", ":noh<CR>", { desc = "Clear search results" })
 vim.keymap.set("n", "<leader>ce", ":e " .. vim.fn.expand("$HOME/.config/nvim/") .. "<CR>", { desc = "Open config dir" })
